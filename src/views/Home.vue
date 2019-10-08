4899e8bda941122cdf7cc33e9c5b119d4dbedb83
ToriK17 <vfcad@protonmail.com>
2019-10-08 14:14:28 -0700

    adds initial commit with REST routes, tested and working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,87 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Add another Place</h1>
+    <div>
+      Name: <input type="text" v-model="newPlaceName">
+      Address: <input type="text" v-model="newPlaceAddress">
+      <button v-on:click="createPlace()">Terraform this bitch</button>
+    </div>
+
+    <h1>All Places</h1>
+    <div v-for="place in places">
+      <h3>{{place.name}}</h3>
+      <button v-on:click="showPlace(place)">Show address</button>
+      <div v-if="currentPlace === place">
+        <h3>{{place.address}}</h3>
+        <div>
+          Name: <input type="text" v-model="place.name">
+          Address: <input type="text" v-model="place.address">
+          <button v-on:click="updatePlace(place)">Update Place</button>
+          <button v-on:click="destroyPlace(place)">Wipe from the face of the Map</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      places: [], 
+      currentPlace: {},
+      newPlaceName: "", 
+      newPlaceAddress: ""
+    };
+  },
+  created: function() {
+    axios.get("/api/places").then(response => {
+      this.places = response.data;
+    });
+  },
+  methods: {
+    createPlace: function() {
+      var params = {
+        name: this.newPlaceName, 
+        address: this.newPlaceAddress
+      };
+      axios.post("/api/places", params).then(response => {
+        this.places.push(response.data);
+        this.newPlaceName = "";
+        this.newPlaceAddress = "";
+      });
+    },
+    showPlace: function(place) {
+      if (this.currentPlace === place) {
+        this.currentPlace = {};
+      } else {
+        this.currentPlace = place;
+      }
+    }, 
+    updatePlace: function(place) {
+      var params = {
+        name: place.name, 
+        address: place.address
+      };
+      axios
+        .patch("/api/places/" + place.id, params)
+        .then(response => {
+          this.currentPlace = {};
+        });
+    }, 
+    destroyPlace: function(place) {
+      axios
+        .delete("/api/places/" + place.id)
+        .then(response => {
+          var index = this.places.indexOf(place);
+          this.places.splice(index, 1);
+        });
+    }
   }
-}
+};
 </script>
